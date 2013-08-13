9cbb6999e001c1e94b616795d02c25d3afdd2495
nmorse <morse.nate@gmail.com>
2013-08-13 12:27:42 -0400

    cleaner broken out lists

---
+++ server/cache_server_list.js
@@ -0,0 +1,8 @@
+// Cache Server list
+module.exports = [{"name":"cache1", "example_domain":"some-varnish-cache-1.server.com"}, 
+                    {"name":"cache2", "example_domain":"my-varnish-cache-2.server.com"}, 
+                    {"name":"cache3", "example_domain":"my-varnish-cache-3.server.com"},
+                    {"name":"cache4", "example_domain":"my-varnish-cache-4.server.com"},
+                    {"name":"cache5", "example_domain":"my-varnish-cache-5.server.com"},
+                    {"name":"cache6", "example_domain":"my-varnish-cache-6.server.com"}
+                   ];
